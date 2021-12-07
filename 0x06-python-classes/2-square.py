db0dac1323439bae901462d69f3c7dd08fc27836
homemix <kenmutati@gmail.com>
2021-12-07 16:16:20 +0300

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """An empty Square class
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square class with attribute size.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
