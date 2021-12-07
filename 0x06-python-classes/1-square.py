9b8224a4fa1485a2bfe63427a6e92337b5ca2930
homemix <kenmutati@gmail.com>
2021-12-07 15:47:09 +0300

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """Square class of a quadrilateral with four equal sides
-           Attributes:
-               Size 
-        """
-    def __init__(self, size):
-        self.__size = size
+    """An empty Square class"""
+    def __init__(self,size):
+        self.size = size
