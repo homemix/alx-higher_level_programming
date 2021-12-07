5f7f6782996c39c5eed3e2130fc3ad29332c6821
homemix <kenmutati@gmail.com>
2021-12-07 15:42:24 +0300

    0. edit qst 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Square Class
-This class don't do anything for the moment
-"""
+"""Documentation for a square class"""
 
 
-class Square:
-    pass
-
+class Square():
+    """Square class of a quadrilateral with four equal sides
+       Attributes:
+           None
+    """
+    
     
