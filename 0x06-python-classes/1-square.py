3c0b0d48f958049370d7b64da9291775bc01a58c
homemix <kenmutati@gmail.com>
2021-12-07 15:32:16 +0300

    0. My first square edit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    """Square class of a quadrilateral with four equal sides
+           Attributes:
+               Size
+        """
     def __init__(self, size):
         self.__size = size
