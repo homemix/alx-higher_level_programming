93d879f91245cf5f919c9d0567a826c95dd50697
homemix <kenmutati@gmail.com>
2021-12-13 20:43:55 +0300

    0. Simple rectangle edit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""
+a class representing a rectangle
+"""
+
+
 class Rectangle():
-    """ A class representing rectangles.  """
 
     def __init__(self):
         pass
-
-   
