72bad3e182fb9fd25b79f352f2e34eb376cf9f1e
homemix <kenmutati@gmail.com>
2021-12-20 17:53:26 +0300

    5. Geometry module

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,10 +1,11 @@
 # !/usr/bin/python3
-
 """
 a base class for geometry
 """
 
 
 class BaseGeometry:
-    """a base class for geometry """
+    """
+    a base class for geometry 
+    """
     pass
