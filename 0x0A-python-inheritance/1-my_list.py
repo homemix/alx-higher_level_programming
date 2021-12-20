97acb221bdf5c41c3dbe098c38ac285c5ef0d9a2
homemix <kenmutati@gmail.com>
2021-12-20 17:22:30 +0300

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,10 +7,6 @@ A class to print sored list
 class MyList(list):
     """ a class to print a list"""
 
-    def __init__(self):
-        """ initialize the class"""
-        super().__init__()
-
     def print_sorted(self):
         """ print the sorted list"""
         print(sorted(self))
