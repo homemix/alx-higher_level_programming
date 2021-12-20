f7180f79e9bc957259ef5123847f69040af793d1
homemix <kenmutati@gmail.com>
2021-12-20 16:08:48 +0300

    remove file

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,7 @@
 # !/usr/bin/python3
-def is_kind_of_class(obj, a_class):
-    """returns True if 
-    the object is an instance of, or 
-    if the object is an instance of a class
-     that inherited from, the specified class"""
-    if isinstance(obj, a_class):
-        return True
-    return False
+def is_same_class(obj, a_class):
+    """
+     function that returns True if the object is exactly an 
+     instance of the specified class ; otherwise False.
+    """
+    return type(obj) is a_class
