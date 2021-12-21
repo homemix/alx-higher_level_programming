a137637b2811b1c16894a994ff67051bfdcc419b
homemix <kenmutati@gmail.com>
2021-12-21 14:43:12 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 def read_file(filename=""):
     """ A function to raed a file and print it to  output"""
-    with open(filename, mode='r',encoding='utf-8') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         print(f.read())
