c4439a6a5189dd2b0d5f20c46b16d0cedcac6718
homemix <kenmutati@gmail.com>
2021-12-21 15:38:24 +0300

    3. To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import json
 
+""" a function to serialize an object  """
+
 
 def to_json_string(my_obj):
-    """ a function to serialize an object"""
+    """Returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
