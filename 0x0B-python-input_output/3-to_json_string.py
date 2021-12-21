e58d58c581ea98cff11b098260d027adbe9b858f
homemix <kenmutati@gmail.com>
2021-12-21 15:34:12 +0300

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+""" a function to serialize an object"""
+
+
+def to_json_string(my_obj):
+    """ a function to serialize an object"""
+    return json.dumps(my_obj)
