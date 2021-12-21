042393d740a24255499aef02ca4faee14ac038c3
homemix <kenmutati@gmail.com>
2021-12-21 14:51:37 +0300

    0. Read file edit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" a function that reads a text file (UTF8) and prints it to stdout:"""
+""" 
+a function that reads a text file (UTF8) and prints it to stdout:
+"""
 
 
 def read_file(filename=""):
-    """ A function to raed a file and print it to  output"""
+    """ 
+    A function to raed a file and print it to  output
+    """
     with open(filename, encoding="utf-8", mode="r") as f:
         print(f.read(), end='')
