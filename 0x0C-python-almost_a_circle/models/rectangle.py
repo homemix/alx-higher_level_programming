a0a0b5fa5ab02852e5edada7921f81ec87638249
homemix <kenmutati@gmail.com>
2022-01-01 12:15:04 +0300

    12 square update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
         return "{} ({}) {}/{} - {}/{}".format(self.__class__.__name__, self.id, self.__x, self.__y, self.__width,
                                               self.__height)
 
-    def update(self, *args):
+    def update(self, *args,**kwargs):
         """ update the rectangle with keyword and non keyword arguments."""
         if len(kwargs) != 0:
             for k, v in kwargs.items():
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ from models import Rectangle
 
 class Square(Rectangle):
     """ initialize the class square"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """ initialize the class square with inherited properties"""
         super().__init__(id=id, x=x, y=y, width=size, height=size)
@@ -25,3 +26,17 @@ class Square(Rectangle):
     def __str__(self):
         """ str representation of square class"""
         return "{} ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ update the square with keyword and non keyword arguments."""
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        elif len(args) != 0:
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
