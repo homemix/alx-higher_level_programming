0ed0da031c20c616e5a2481c2480ea1174163e66
homemix <kenmutati@gmail.com>
2022-01-01 13:00:55 +0300

    18. Dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,9 +22,24 @@ class Base:
         return json.dumps(list_dictionaries)
 
     def save_to_file(cls, list_objs):
-        with open(cls.__name__+'.json', 'w') as f:
+        """ save json dictionary to file"""
+        with open(cls.__name__ + '.json', 'w') as f:
             if list_objs is None:
                 f.write('[]')
             else:
                 f.write(cls.to_json_string())
-        
+
+    def from_json_string(json_string):
+        """convert to json file and return"""
+        if len(json_string) == 0 or json_string is None:
+            return '[]'
+        return json.dumps(json_string)
+
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
