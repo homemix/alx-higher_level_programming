4fabc6a7a127be95ecb1103b969a462997e5c01f
homemix <kenmutati@gmail.com>
2022-02-01 18:56:06 +0300

    15. Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 --  a script that lists the number of records with the same score in the table second_table of the database
 SELECT `score`, COUNT(`score`) AS number FROM `second_table`
-GROUP BY `score` ORDER BY `number`;
+GROUP BY `score` ORDER BY `number` DESC ;
