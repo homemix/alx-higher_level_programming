e581ab417390aebac1cf053deeac8745467d2980
homemix <kenmutati@gmail.com>
2022-03-04 18:43:49 +0300

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa 
+where name matches the argument
 """
 
 import MySQLdb
@@ -10,7 +11,8 @@ import sys
 db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
+
 rows = cursor.fetchall()
 for row in rows:
     print(row)
