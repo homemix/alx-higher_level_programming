8763740c0d3d48170be52403ec21bb747c65c159
homemix <kenmutati@gmail.com>
2022-03-05 11:12:51 +0300

    a script that lists all states from the database hbtn_0e_0_usa:

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""
+a script that takes in the name of a state as an argument and lists all cities of that state,
+ using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cursor = db.cursor()
+cursor.execute(
+    "select cities.name from cities  INNER join states on  states.id = cities.state_id where states.name = '{}'".format((sys.argv[4])))
+
+rows = cursor.fetchall()
+#  print rows with comma separated values except the last one
+for row in rows:
+    print(row[0], end=", ")
+print("\b\b")
+cursor.close()
