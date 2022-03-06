7bd6d0383fe6af8aba236926cd76a86545c3529a
homemix <kenmutati@gmail.com>
2022-03-06 14:11:18 +0300

    pycodestyle format qst 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,17 +2,21 @@
 
 
 """
-a script that takes in the name of a state as an argument and lists all cities of that state,
+a script that takes in the name of a state as
+n argument and lists all cities of that state,
  using the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute(
-        "select cities.name from cities  INNER join states on  states.id = cities.state_id where states.name like %s order by cities.id",
+        "select cities.name from cities  INNER join states"
+        " on  states.id = cities.state_id "
+        "where states.name like %s order by cities.id",
         (sys.argv[4],))
 
     rows = cursor.fetchall()
