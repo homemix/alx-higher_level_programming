85c3f783602d36774d2b3ddde63ef38b7aadda11
homemix <kenmutati@gmail.com>
2022-03-06 14:23:06 +0300

    class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
+"""
+contains the class definition of a State and an instance Base
+"""
 
-"""
- a python file that contains the
- class definition of a State and an instance Base = declarative_base()
-"""
 import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format("root", "root", "my_db"), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-session = Session(engine)
+Base = declarative_base()
 
 
 class State(Base):
-    """
-    class State that inherits from Base
-    """
+    """Representation of a state"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True,
-                autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
