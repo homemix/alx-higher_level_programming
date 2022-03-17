642401fdb8494ff1acd4e69317a668b1a7b573f9
homemix <kenmutati@gmail.com>
2022-03-17 20:41:41 +0300

    Bash script that takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display allowed http method
-curl -sI "$1" -X OPTIONS | grep "Allow" | cut -c 8-
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow: " | sed 's/Allow: //'
