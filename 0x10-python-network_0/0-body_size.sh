1a349c1bb953cd6b8aaba7b3ee9599c8d836de7f
homemix <kenmutati@gmail.com>
2022-03-17 17:19:03 +0300

    script that takes in a URL, sends a request to that URL, and displays the size of the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# script that  takes in a URL, sends a request to that URL, and displays the size of the body
+# script that  take in a URL, sends a request to that URL, and displays the size of the body
 
 curl -sI "$1" | grep Content-Length | cut -d " " -f2
 
