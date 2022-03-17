5558d360faef16f49ade3b2db2f538144969d227
homemix <kenmutati@gmail.com>
2022-03-17 20:51:05 +0300

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""
+find the peek
+"""
 
 
-def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+def find_peak(arr):
+    """
+        find the peek
+    """
+    if not arr:
+        return None
+    if len(arr) == 1:
+        return arr[0]
+    if arr[0] >= arr[1]:
+        return arr[0]
+    if arr[len(arr) - 1] >= arr[len(arr) - 2]:
+        return arr[len(arr) - 1]
+    for i in range(1, len(arr) - 1):
+        if arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]:
+            return arr[i]
