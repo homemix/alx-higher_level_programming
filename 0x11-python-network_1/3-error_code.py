d9813ff81badd36fd24d3b35a5026630552fafce
homemix <kenmutati@gmail.com>
2022-03-18 17:52:01 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the
-response (decoded in utf-8)
-"""
+"""Send request"""
 import urllib.request
-from sys import argv
+import urllib.error
+import sys
 
-if __name__ == "__main__":
-    """
-    takes in a URL and an email, sends a POST request to the passed
-    URL with the email as a parameter, and displays the body of the
-    response (decoded in utf-8)
-    """
-    url = argv[1]
-    req = urllib.request.Request(url)
+
+def sender():
+    """sender"""
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             html = response.read()
-            html_str = html.decode('utf-8')
-            print(html_str)
+            print(html.decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    sender()
