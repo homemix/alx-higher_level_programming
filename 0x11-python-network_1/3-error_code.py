2413ac4f5089032f3d17a9f470e9d2b25390432f
homemix <kenmutati@gmail.com>
2022-03-18 17:55:47 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Send request"""
 import urllib.request
-import urllib.error
-import sys
+#!/usr/bin/python3
+"""
+request a body if error display statue code
+"""
 
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+    url = sys.argv[1]
 
-def sender():
-    """sender"""
+    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode("utf-8"))
+        with urllib.request.urlopen(req) as response:
+            info = response.read()
+            print(info.decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
-
-
-if __name__ == "__main__":
-    sender()
